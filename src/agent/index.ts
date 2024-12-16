594670ddc23ab4ce9a6b1437847d214301e06fde
adolphzhang <adolphzhang@live.com>
2024-12-16 20:25:11 +0800

    style: add type annotations.

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -180,12 +180,13 @@ export async function createLlmModel(model: string, context: AgentUserConfig) {
     if (!model_id) {
         model_id = context[`${agent.toUpperCase()}_CHAT_MODEL`];
     }
-    const GOOGLE_SAFETY = [
+    const GOOGLE_SAFETY: { category: 'HARM_CATEGORY_UNSPECIFIED' | 'HARM_CATEGORY_DANGEROUS_CONTENT' | 'HARM_CATEGORY_HARASSMENT' | 'HARM_CATEGORY_HATE_SPEECH' | 'HARM_CATEGORY_SEXUALLY_EXPLICIT' | 'HARM_CATEGORY_CIVIC_INTEGRITY'; threshold: 'BLOCK_NONE' | 'BLOCK_LOW_AND_ABOVE' | 'BLOCK_MEDIUM_AND_ABOVE' | 'BLOCK_ONLY_HIGH' }[] = [
         { category: 'HARM_CATEGORY_UNSPECIFIED', threshold: 'BLOCK_NONE' },
         { category: 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold: 'BLOCK_NONE' },
         { category: 'HARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE' },
         { category: 'HARM_CATEGORY_HATE_SPEECH', threshold: 'BLOCK_NONE' },
-        { category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT', threshold: 'BLOCK_NONE' }];
+        { category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT', threshold: 'BLOCK_NONE' },
+    ];
 
     switch (agent) {
         case 'openai':
